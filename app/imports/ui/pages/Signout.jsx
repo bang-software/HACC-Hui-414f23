f89d2cc9d13fe28fcf75e7fd13ec0842e38547bb
gitCarrot <jeonhg0@gmail.com>
2023-09-05 22:19:59 -1000

    Convert to Bootstrap

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header } from 'semantic-ui-react';
 
 /**
  * After the user clicks the "Signout" link in the NavBar, log them out and display this page.
@@ -10,9 +9,9 @@ class Signout extends React.Component {
   render() {
     Meteor.logout();
     return (
-      <Header as="h2" textAlign="center">
-        <p>You are signed out.</p>
-      </Header>
+        <div className="text-center my-4">
+          <h2>You are signed out.</h2>
+        </div>
     );
   }
 }
