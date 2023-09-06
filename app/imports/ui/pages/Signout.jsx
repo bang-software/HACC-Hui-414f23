7e064f5d3ec9e6b1f9a2a061e35b9a9d75606ee3
gitCarrot <jeonhg0@gmail.com>
2023-09-05 23:37:57 -1000

    convert from class to function for the component

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -5,15 +5,13 @@ import { Container } from 'react-bootstrap';
  * After the user clicks the "Signout" link in the NavBar, log them out and display this page.
  * @memberOf ui/pages
  */
-class Signout extends React.Component {
-  render() {
+const Signout = () => {
     Meteor.logout();
     return (
         <Container id={"signout-page"} className="text-center my-4">
-          <h2>You are signed out.</h2>
+            <h2>You are signed out.</h2>
         </Container>
     );
-  }
 }
 
 export default Signout;
