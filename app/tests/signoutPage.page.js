78c35b76bded667f846c57cdc5f9ff74261c9528
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-09-06 07:37:37 -1000

    Completed signout testcafe

--- app/tests/signoutPage.page.js
+++ app/tests/signoutPage.page.js
@@ -10,21 +10,6 @@ class SignOutPage {
   async isDisplayed(testController) {
     await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
-
-  /** Fills out and submits the form to sign-out, then checks to see that login was successful. */
-
-  async checkSignout(testController) {
-    const loggedInUser = await Selector('#navbar-current-user').exists;
-    if (!loggedInUser) {
-      await testController.click('#navbar-sign-in');
-    }
-  }
-
-  async signout(testController) {
-    await this.isDisplayed(testController);
-    await this.checkSignout(testController);
-  }
-
 }
 
 export const signOutPage = new SignOutPage();
