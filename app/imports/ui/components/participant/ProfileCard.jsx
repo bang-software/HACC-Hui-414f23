edf92747279dc29d6e7eda4c024cadcffd72cc13
yongsungm <97987234+yongsungm@users.noreply.github.com>
2023-09-14 18:44:57 -1000

    Got rid of semantic-ui
    Still needs work

--- app/imports/ui/components/participant/ProfileCard.jsx
+++ app/imports/ui/components/participant/ProfileCard.jsx
@@ -1,67 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Grid, Header, Icon, List } from 'semantic-ui-react';
+import { Card, Col, Row, Container, Image, ListGroup } from 'react-bootstrap';
 import SkillItem from './SkillItem';
 import ToolItem from './ToolItem';
 
-class ProfileCard extends React.Component {
-  render() {
-    return (
-        <Card fluid>
-          <Card.Content>
-            <Card.Header>{this.props.model.firstName} {this.props.model.lastName}</Card.Header>
-            <Card.Meta>
-              {this.props.model.username} <br /> {this.props.model.demographicLevel}
-            </Card.Meta>
-            <Card.Description>
-              <Grid container stackable columns={4}>
-                <Grid.Column><Icon name="github" /> GitHub:<br />
-                  <a href={this.props.model.gitHub}>{this.props.model.gitHub}</a>
-                </Grid.Column>
-                <Grid.Column><Icon name="linkedin" /> Linked In:<br />
-                  <a href={this.props.model.linkedIn}>{this.props.model.linkedIn}</a>
-                </Grid.Column>
-                <Grid.Column><Icon name="server" /> Website:<br />
-                  <a href={this.props.model.website}>{this.props.model.website}</a>
-                </Grid.Column>
-                <Grid.Column><Icon name="slack" /> Slack:<br />
-                  <a href={this.props.model.slackUsername}>{this.props.model.slackUsername}</a>
-                </Grid.Column>
-              </Grid>
-              <Grid container columns={1}>
-                <Grid.Column><Icon name="comment" /> About me: {this.props.model.aboutMe}</Grid.Column>
-              </Grid>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content>
-            <Grid container stackable columns={3}>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header dividing size="small">Challenges</Header>
-                  <List bulleted>
-                  {this.props.model.challenges.map((item) => <List.Item key={item}>{item}</List.Item>)}
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header dividing size="tiny">Skills</Header>
-                  <List bulleted>
-                    {this.props.model.skills.map((item) => <SkillItem item={item} key={item._id} />)}
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header dividing size="tiny">Tools</Header>
-                  <List bulleted>
-                    {this.props.model.tools.map((item) => <ToolItem item={item} key={item._id} />)}
-                  </List>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Card.Content>
-        </Card>
-    );
-  }
+const ProfileCard = ({ model }) => (
+  // <Card>
+  //   <Card.Body>
+  //     <Card.Header>
+  //       <h1>{ model.firstName} {model.lastName}</h1>
+  //     </Card.Header>
+  //     <Card.Subtitle>{ model.username }<br/> { model.demographicLevel }</Card.Subtitle>
+  //     <Container>
+  //       <Col>
+  //         {model.username}
+  //       </Col>
+  //     </Container>
+  //   </Card.Body>
+  // </Card>
 
-}
+  <Card>
+    <Card.Body>
+      <Card.Header>
+        <h1>{model.firstName} {model.lastName}</h1>
+      </Card.Header>
+      <Card.Subtitle>{ model.username }<br/> { model.demographicLevel }
+      </Card.Subtitle>
+      <Container>
+        <Col>
+          {/*<Image width='300px' src='/images/HACC_icon_2022.png' /><Icon name="github"/> GitHub:<br/>*/}
+          <a href={model.gitHub}>{model.gitHub}</a>
+        </Col>
+        <Col>
+          {/*<Icon name="linkedin"/> Linked In:<br/>*/}
+          <a href={model.linkedIn}>{model.linkedIn}</a>
+        </Col>
+        <Col>
+          {/*<Icon name="server"/> Website:<br/>*/}
+          <a href={model.website}>{model.website}</a>
+        </Col>
+        <Col>
+          {/*<Icon name="slack"/> Slack:<br/>*/}
+          <a href={model.slackUsername}>{model.slackUsername}</a>
+        </Col>
+      </Container>
+        {/*<Container container columns={1}>*/}
+        {/*  <Col>*/}
+        {/*    <Icon name="comment"/> About me: {model.aboutMe}*/}
+        {/*  </Col>*/}
+        {/*</Container>*/}
+    </Card.Body>
+    <Card.Body>
+      <Container>
+        <Row>
+          <Col>
+            <h4>Challenges</h4>
+            <ListGroup>
+              {model.challenges.map((item) => <ListGroup.Item key={item}>{item}</ListGroup.Item>)}
+            </ListGroup>
+          </Col>
+          <Col>
+            <h6>Skills</h6>
+            <ListGroup>
+              {model.skills.map((item) => <SkillItem item={item} key={item._id}/>)}
+            </ListGroup>
+          </Col>
+          <Col>
+            <h6>Tools</h6>
+            <ListGroup>
+              {model.tools.map((item) => <ToolItem item={item} key={item._id}/>)}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </Card.Body>
+  </Card>
+);
 
 ProfileCard.propTypes = {
   model: PropTypes.object.isRequired,
