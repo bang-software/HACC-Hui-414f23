d30d21fc11592d423117f7e293130b63ce41d25e
Jericho Macabante <jerichomacabante@gmail.com>
2023-09-19 21:46:36 -1000

    Start ParticipationFormBootstrap.jsx.

--- app/imports/ui/pages/participant/ParticipationFormBootstrap.jsx
+++ app/imports/ui/pages/participant/ParticipationFormBootstrap.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Form, Header, Message, Segment } from 'semantic-ui-react';
+import { Button, Form, Alert } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
-import { AutoForm, BoolField, SubmitField, TextField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { ROUTES } from '../../../startup/client/route-constants';
@@ -11,6 +10,7 @@ import { updateMethod } from '../../../api/base/BaseCollection.methods';
 import { userInteractionDefineMethod } from '../../../api/user/UserInteractionCollection.methods';
 import { USER_INTERACTIONS } from '../../../startup/client/user-interaction-constants';
 import { darkerBlueStyle } from '../../styles';
+import { PAGE_IDS } from '../../testIDs/pageIDs'
 
 const schema = new SimpleSchema({
   lastName: String,
@@ -22,13 +22,9 @@ const schema = new SimpleSchema({
  * A simple static component to render some text for the landing page.
  * @memberOf ui/pages
  */
-class ParticipationForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { redirectToReferer: false };
-  }
-
-  submit(formData) {
+export default function ParticipationForm() {
+  const [redirectToReferer, setRedirectToReferer] = useState(false);
+  const submit = (formData) => {
     const { firstName, lastName, agree } = formData;
     if (agree) {
       const dev = Participants.findDoc({ userID: Meteor.userId() });
@@ -53,37 +49,33 @@ class ParticipationForm extends React.Component {
           console.error('Could not define user interaction', error);
         }
       });
-      this.setState({ redirectToReferer: true });
+      setRedirectToReferer(true);
     }
+  };
+
+  if (redirectToReferer) {
+    const from = { pathname: ROUTES.CREATE_PROFILE };
+    return <Redirect to={from}/>;
   }
 
-  render() {
-    const formSchema = new SimpleSchema2Bridge(schema);
-    if (this.state.redirectToReferer) {
-      const from = { pathname: ROUTES.CREATE_PROFILE };
-      return <Redirect to={from}/>;
-    }
-    return (
-        <Segment style={darkerBlueStyle}>
-          <Header>HACC Registration</Header>
-          <AutoForm schema={formSchema} onSubmit={data => this.submit(data)}>
-              <Segment>
-                <Message>
-                  Read the <a href="https://hacc.hawaii.gov/hacc-rules/">HACC Rules</a>.
-                  <br />
-                  Then agree to the terms.
-                </Message>
-                <Form.Group widths="equal">
-                  <TextField name="firstName" />
-                  <TextField name="lastName" />
-                </Form.Group>
-                <BoolField name="agree" label="I have read the rules and agree to the terms" />
-                <SubmitField />
-              </Segment>
-            </AutoForm>
-        </Segment>
-    );
-  }
+  return (
+      <div id={PAGE_IDS.PARTICIPATION_FORM} style={darkerBlueStyle}>
+        <h1>HACC Registration</h1>
+        <Form onSubmit={(e) => {
+          e.preventDefault();
+          const formData = {
+            firstName: e.target.firstName.value,
+            lastName: e.target.lastName.value,
+            agree: e.target.agree.checked,
+          };
+        }}>
+        <Alert variant="info">
+          Read the <a href="https://hacc.hawaii.gov/hacc-rules/">HACC Rules</a>
+          <br />
+          Then agree to the terms
+        </Alert>
+        </Form>
+      </div>
+  );
 }
 
-export default ParticipationForm;
