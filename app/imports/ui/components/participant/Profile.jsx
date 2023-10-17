448bc356d2edceb87854a4ee25874892591fa1cf
yongsungm <97987234+yongsungm@users.noreply.github.com>
2023-10-16 14:47:11 -1000

    Update Profile.jsx
    - Profile now uses useTracker
    - Profile accesses collection data and passes it to its children so they don't have to access collection data anymore.

--- app/imports/ui/components/participant/Profile.jsx
+++ app/imports/ui/components/participant/Profile.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { withRouter, Link } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Button, Container } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import { Button, Container, Spinner } from 'react-bootstrap';
 import { Participants } from '../../../api/user/ParticipantCollection';
 import { Challenges } from '../../../api/challenge/ChallengeCollection';
 import { ParticipantChallenges } from '../../../api/user/ParticipantChallengeCollection';
@@ -16,21 +15,36 @@ import TeamMembershipWidget from './TeamMembershipWidget';
 import { Tools } from '../../../api/tool/ToolCollection';
 import { Skills } from '../../../api/skill/SkillCollection';
 
-const Profile = ({ participant, devChallenges, skillNames, toolNames }) => {
-
-  const buildModel = () => {
-    const model = participant;
-    model.challenges = devChallenges.map((challenge) => {
-      const c = Challenges.findDoc(challenge.challengeID);
-      return c.title;
-    });
-    model.skills = skillNames;
-    model.tools = toolNames;
-    return model;
-  };
-
-  const model = buildModel();
-  return (
+const Profile = () => {
+    const { model, ready } = useTracker(() => {
+      const sub2 = ParticipantChallenges.subscribe();
+      const sub3 = ParticipantSkills.subscribe();
+      const sub4 = ParticipantTools.subscribe();
+      const sub5 = Challenges.subscribe();
+      const sub6 = Skills.subscribe();
+      const sub7 = Tools.subscribe();
+      const rdy = sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready() && sub6.ready() && sub7.ready();
+      const participant = Participants.findDoc({ userID: Meteor.userId() });
+      const participantID = participant._id;
+      const challengeTitles = ParticipantChallenges.find({ participantID }).fetch().map(
+        (challenge) => Challenges.findDoc(challenge.challengeID).title,
+        );
+      const skillNames = ParticipantSkills.find({ participantID }).fetch().map(
+        (skill) => Skills.findDoc(skill.skillID).name,
+      );
+      const toolNames = ParticipantTools.find({ participantID }).fetch().map(
+        (tool) => Tools.findDoc(tool.toolID).name,
+      );
+      const tempModel = participant;
+      tempModel.challenges = challengeTitles;
+      tempModel.skills = skillNames;
+      tempModel.tools = toolNames;
+      return {
+        model: tempModel,
+        ready: rdy,
+      };
+    }, []);
+  return (ready ? (
     <div style={{ paddingBottom: '50px', paddingTop: '40px' }}><Container>
       <div style={paleBlueStyle}>
         <h2>Your Profile</h2>
@@ -45,44 +59,7 @@ const Profile = ({ participant, devChallenges, skillNames, toolNames }) => {
       </div>
     </Container>
     </div>
-  );
+  ) : <Spinner/>);
 };
 
-Profile.propTypes = {
-  participant: PropTypes.object.isRequired,
-  devChallenges: PropTypes.arrayOf(
-    PropTypes.object,
-  ),
-  devSkills: PropTypes.arrayOf(
-    PropTypes.object,
-  ),
-  devTools: PropTypes.arrayOf(
-    PropTypes.object,
-  ),
-  toolNames: PropTypes.arrayOf(
-    PropTypes.string,
-  ),
-  skillNames: PropTypes.arrayOf(
-    PropTypes.string,
-  ),
-};
-
-const ProfileCon = withTracker(() => {
-  const participant = Participants.findDoc({ userID: Meteor.userId() });
-  const participantID = participant._id;
-  const devChallenges = ParticipantChallenges.find({ participantID }).fetch();
-  const devSkills = ParticipantSkills.find({ participantID }).fetch();
-  const devTools = ParticipantTools.find({ participantID }).fetch();
-  const skillNames = devSkills.map((skill) => Skills.findDoc(skill.skillID).name);
-  const toolNames = devTools.map((tool) => Tools.findDoc(tool.toolID).name);
-  return {
-    participant,
-    devChallenges,
-    devSkills,
-    devTools,
-    toolNames,
-    skillNames,
-  };
-})(Profile);
-
-export default withRouter(ProfileCon);
+export default withRouter(Profile);
