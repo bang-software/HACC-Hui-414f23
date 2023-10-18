502022ec5e99b355d4ed86012ad3793ac187fc63
Jericho Macabante <jerichomacabante@gmail.com>
2023-10-17 22:06:54 -1000

    Working on YourTeams.

--- app/imports/ui/components/participant/YourTeamsWidget.jsx
+++ app/imports/ui/components/participant/YourTeamsWidget.jsx
@@ -21,94 +21,90 @@ import { paleBlueStyle } from '../../styles';
  * Widget to list teams
  * @memberOf ui/pages
  */
-class YourTeamsWidget extends React.Component {
+const YourTeamsWidget = () => {
+  const allParticipants =
 
-  render() {
-
-    const allParticipants = this.props.participants;
-
-    function getTeamParticipants(teamID, teamParticipants) {
-      const data = [];
-      const participants = _.uniqBy(_.filter(teamParticipants, { teamID: teamID }), 'participantID');
-      // console.log(participants);
-      for (let i = 0; i < participants.length; i++) {
-        for (let j = 0; j < allParticipants.length; j++) {
-          if (participants[i].participantID === allParticipants[j]._id) {
-            data.push({
-              firstName: allParticipants[j].firstName,
-              lastName: allParticipants[j].lastName,
-            });
-          }
+  function getTeamParticipants(teamID, teamParticipants) {
+    const data = [];
+    const participants = _.uniqBy(_.filter(teamParticipants, { teamID: teamID }), 'participantID');
+    // console.log(participants);
+    for (let i = 0; i < participants.length; i++) {
+      for (let j = 0; j < allParticipants.length; j++) {
+        if (participants[i].participantID === allParticipants[j]._id) {
+          data.push({
+            firstName: allParticipants[j].firstName,
+            lastName: allParticipants[j].lastName,
+          });
         }
       }
-      return data;
     }
+    return data;
+  }
 
-    if (!this.props.participant.isCompliant) {
-      return (
-          <div align={'center'}>
-            <Header as='h2' icon>
-              <Icon name='thumbs down outline' />
-              You have not agreed to the <a href="https://hacc.hawaii.gov/hacc-rules/">HACC Rules</a>
-              &nbsp;or we&apos;ve haven&apos;t received the signed form yet.
-              <Header.Subheader>
-                You can&apos;t be the owner of a team until you do. Please check back later.
-              </Header.Subheader>
-            </Header>
-          </div>
-      );
-    }
-    if (this.props.teams.length + this.props.memberTeams.length === 0) {
-      return (
-          <div align={'center'}>
-            <Header as='h2' icon>
-              <Icon name='users' />
-              You are not the owner or member of any teams
-              <Header.Subheader>
-                Please check back later.
-              </Header.Subheader>
-            </Header>
-          </div>
-      );
-    }
-
+  if (!this.props.participant.isCompliant) {
     return (
-        <Grid container doubling relaxed stackable style={{ paddingBottom: 50 }}>
-          <Grid.Row centered>
-            <Header as='h2' textAlign="center" style={{ paddingBottom: '1rem' }}>
-              Your Teams
-            </Header>
-          </Grid.Row>
-          {this.props.teams.length === 0 ? '' : (
-              <Grid.Column width={15}>
-                <Segment style={paleBlueStyle}><Header as="h4" textAlign="center">Owner</Header>
-                  <Item.Group divided>
-                    {/* eslint-disable-next-line max-len */}
-                    {this.props.teams.map((teams) => <YourTeamsCard key={teams._id} teams={teams}
-                                                                    teamParticipants={getTeamParticipants(teams._id,
-                                                                        this.props.teamParticipants)}
-                                                                    teamInvitation={this.props.teamInvitation} />)}
-                  </Item.Group></Segment>
-              </Grid.Column>)
-          }
-          {this.props.memberTeams.length === 0 ? '' : (
-              <Grid.Column width={15}>
-                <Segment><Header as="h4" textAlign="center">Member</Header>
-                  <Item.Group divided>
-                    {this.props.memberTeams.map((team) => <MemberTeamCard key={team._id}
-                                                                          team={team}
-                                                                          teamParticipants={getTeamParticipants(
-                                                                              team._id,
-                                                                              this.props.teamParticipants,
-                                                                          )} />)}
-                  </Item.Group></Segment>
-              </Grid.Column>
-          )
-          }
-        </Grid>
+        <div>
+          <Header as='h2' icon>
+            <Icon name='thumbs down outline' />
+            You have not agreed to the <a href="https://hacc.hawaii.gov/hacc-rules/">HACC Rules</a>
+            &nbsp;or we&apos;ve haven&apos;t received the signed form yet.
+            <Header.Subheader>
+              You can&apos;t be the owner of a team until you do. Please check back later.
+            </Header.Subheader>
+          </Header>
+        </div>
     );
   }
-}
+  if (this.props.teams.length + this.props.memberTeams.length === 0) {
+    return (
+        <div>
+          <Header as='h2' icon>
+            <Icon name='users' />
+            You are not the owner or member of any teams
+            <Header.Subheader>
+              Please check back later.
+            </Header.Subheader>
+          </Header>
+        </div>
+    );
+  }
+
+  return (
+      <Grid container doubling relaxed stackable style={{ paddingBottom: 50 }}>
+        <Grid.Row centered>
+          <Header as='h2' textAlign="center" style={{ paddingBottom: '1rem' }}>
+            Your Teams
+          </Header>
+        </Grid.Row>
+        {this.props.teams.length === 0 ? '' : (
+            <Grid.Column width={15}>
+              <Segment style={paleBlueStyle}><Header as="h4" textAlign="center">Owner</Header>
+                <Item.Group divided>
+                  {/* eslint-disable-next-line max-len */}
+                  {this.props.teams.map((teams) => <YourTeamsCard key={teams._id} teams={teams}
+                                                                  teamParticipants={getTeamParticipants(teams._id,
+                                                                      this.props.teamParticipants)}
+                                                                  teamInvitation={this.props.teamInvitation} />)}
+                </Item.Group></Segment>
+            </Grid.Column>)
+        }
+        {this.props.memberTeams.length === 0 ? '' : (
+            <Grid.Column width={15}>
+              <Segment><Header as="h4" textAlign="center">Member</Header>
+                <Item.Group divided>
+                  {this.props.memberTeams.map((team) => <MemberTeamCard key={team._id}
+                                                                        team={team}
+                                                                        teamParticipants={getTeamParticipants(
+                                                                            team._id,
+                                                                            this.props.teamParticipants,
+                                                                        )} />)}
+                </Item.Group></Segment>
+            </Grid.Column>
+        )
+        }
+      </Grid>
+  );
+};
 
 YourTeamsWidget.propTypes = {
   participant: PropTypes.object.isRequired,
