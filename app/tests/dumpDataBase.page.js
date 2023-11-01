81a9743e413c5df8dae8746fb758b297a81cadde
yongsungm <97987234+yongsungm@users.noreply.github.com>
2023-11-01 04:46:26 -1000

    Update dumpDataBase.page.js

--- app/tests/dumpDataBase.page.js
+++ app/tests/dumpDataBase.page.js
@@ -9,7 +9,7 @@ class DumpDataBasePage {
   }
 
   async isDisplayed(testController) {
-    await testController.wait(30000).expect(this.pageSelector.exists).ok();
+    await testController.wait(5000).expect(this.pageSelector.exists).ok();
   }
 
   async clickDumpDownload(testController) {
