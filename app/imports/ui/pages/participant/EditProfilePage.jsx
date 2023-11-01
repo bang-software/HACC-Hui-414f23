f6d45cd240da3e2ddb7ab770c9b4b074309f0020
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-31 22:44:12 -1000

    Implemented test for edit profile

--- app/imports/ui/pages/participant/EditProfilePage.jsx
+++ app/imports/ui/pages/participant/EditProfilePage.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import EditProfileWidget from '../../components/participant/EditProfileWidget';
+import { PAGE_IDS } from '../../testIDs/pageIDs';
 
 const EditProfilePage = () => (
-    <EditProfileWidget/>
+    <div id={PAGE_IDS.EDIT_PROFILE}>
+      <EditProfileWidget/>
+    </div>
 );
 
 export default withAllSubscriptions(EditProfilePage);
