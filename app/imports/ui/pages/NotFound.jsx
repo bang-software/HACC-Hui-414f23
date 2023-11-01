4d7214568aa48d24732841819b96d51f4a1e4cad
sukaryo-heilscher <sukaryor@hawaii.edu>
2023-11-01 01:13:18 -1000

    Converted NotFound to Bootstrap
    Also have the makings of a test in there but testcafe wasn't a fan of the navigateTo method, so I left it in a comment.

--- app/imports/ui/pages/NotFound.jsx
+++ app/imports/ui/pages/NotFound.jsx
@@ -6,18 +6,14 @@ import { PAGE_IDS } from '../testIDs/pageIDs';
  * Render a Not Found page if the user enters a URL that doesn't match any route.
  * @memberOf ui/pages
  */
-class NotFound extends React.Component {
-  render() {
-    return (
-        <Container id={PAGE_IDS.UPDATE_MP_COMPLIANT}>
-          <Row className="justify-content-center">
-            <Col className="text-center"><h2>
-              Page not found
-            </h2></Col>
-          </Row>
-        </Container>
-    );
-  }
-}
+const NotFound = () => (
+      <Container id={PAGE_IDS.NOT_FOUND}>
+        <Row className="justify-content-center">
+          <Col className="text-center"><h2>
+            Page not found
+          </h2></Col>
+        </Row>
+      </Container>
+  );
 
 export default NotFound;
