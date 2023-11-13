dff3c2fd5dbfb252fb97f1808b6caa2e3396b668
sukaryo-heilscher <sukaryor@hawaii.edu>
2023-11-13 07:36:20 -1000

    WIP

--- app/imports/ui/components/administrator/ManageMinorWidget.jsx
+++ app/imports/ui/components/administrator/ManageMinorWidget.jsx
@@ -1,73 +1,67 @@
 import React from 'react';
-import { Grid, Segment, Header, Table } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
-import { withTracker } from 'meteor/react-meteor-data';
+import { Grid, Header, Segment, Table } from 'semantic-ui-react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Spinner } from 'react-bootstrap';
 import ListMinorWidget from './ListMinorWidget';
 import { MinorParticipants } from '../../../api/user/MinorParticipantCollection';
 import { Participants } from '../../../api/user/ParticipantCollection';
+import { darkGreyStyle, greyStyle } from '../../styles';
 
-/**
- * Renders the Page for Managing HACC. **deprecated**
- * @memberOf ui/pages
- */
-class ManageMinorWidget extends React.Component {
-  render() {
-    // console.log('ManageMinorWidget');
-    return (
-        <div style={{ backgroundColor: '#C4C4C4', paddingBottom: '50px' }}>
-          <Grid container centered>
-            <Grid.Column>
-              <div style={{
-                backgroundColor: '#393B44', padding: '1rem 0rem', margin: '2rem 0rem',
-                borderRadius: '2rem',
-              }}>
-                <Header as="h2" textAlign="center" inverted>Minor Participant</Header>
-                <Header as="h2" textAlign="center" inverted>{this.props.minorParticipants.length}</Header>
-              </div>
-              <Segment style={{
-                borderRadius: '1rem',
-                backgroundColor: '#393B44',
-              }} className={'teamCreate'}>
-                <Header as="h2" textAlign="center" inverted>Information</Header>
-                <Table fixed columns={5}>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell width={2}>Username</Table.HeaderCell>
-                      <Table.HeaderCell width={5}>Parent/Guardian:FirstName</Table.HeaderCell>
-                      <Table.HeaderCell width={5}>Parent/Guardian:LastName</Table.HeaderCell>
-                      <Table.HeaderCell width={5}>Parent/Guardian:Email</Table.HeaderCell>
-                      <Table.HeaderCell width={2}>Approve</Table.HeaderCell>
-                      <Table.HeaderCell width={2}>Delete</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  {/* eslint-disable-next-line max-len */}
-                  <Table.Body>{this.props.minorParticipants.map((minorParticipants => <ListMinorWidget key={minorParticipants._id} minorParticipants={minorParticipants} />
-                  ))}
-                  </Table.Body>
-                </Table>
-              </Segment>
-            </Grid.Column>
-          </Grid>
-        </div>
-    );
-  }
-}
+const ManageMinorWidget = () => {
+  const { ready, minorParticipants } = useTracker(() => {
+    const sub1 = MinorParticipants.subscribe();
+    const sub2 = Participants.subscribe();
+    const rdy = sub1.ready() && sub2.ready();
 
-ManageMinorWidget.propTypes = {
-  minorParticipants: PropTypes.array.isRequired,
+    const mps = MinorParticipants.find({}).fetch();
+    const MPList = [];
+    mps.forEach((m) => {
+      const result = m;
+      result.username = Participants.findDoc(m.participantID).username;
+      MPList.push(result);
+    });
+    return {
+      ready: rdy,
+      minorParticipants: MPList,
+    };
+  }, []);
+
+  return (ready ? (
+      <div style={{ greyStyle, paddingBottom: '50px' }}>
+        <Grid container centered>
+          <Grid.Column>
+            <div style={{
+              darkGreyStyle, padding: '1rem 0rem', margin: '2rem 0rem',
+              borderRadius: '2rem',
+            }}>
+              <Header as="h2" textAlign="center" inverted>Minor Participant</Header>
+              <Header as="h2" textAlign="center" inverted>{minorParticipants.length}</Header>
+            </div>
+            <Segment style={{
+              borderRadius: '1rem',
+              darkGreyStyle,
+            }} className={'teamCreate'}>
+              <Header as="h2" textAlign="center" inverted>Information</Header>
+              <Table fixed columns={5}>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell width={2}>Username</Table.HeaderCell>
+                    <Table.HeaderCell width={5}>Parent/Guardian:FirstName</Table.HeaderCell>
+                    <Table.HeaderCell width={5}>Parent/Guardian:LastName</Table.HeaderCell>
+                    <Table.HeaderCell width={5}>Parent/Guardian:Email</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Approve</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Delete</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>{minorParticipants.map((mp => <ListMinorWidget key={mp._id} minorParticipants={mp}/>
+                ))}
+                </Table.Body>
+              </Table>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </div>
+  ) : <Spinner/>);
 };
 
-// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-export default withTracker(() => {
-  const mps = MinorParticipants.find({}).fetch();
-  const minorParticipants = [];
-  mps.forEach((m) => {
-    const result = m;
-    result.username = Participants.findDoc(m.participantID).username;
-    minorParticipants.push(result);
-  });
-  console.log(minorParticipants);
-  return {
-    minorParticipants,
-  };
-})(ManageMinorWidget);
+export default ManageMinorWidget;
